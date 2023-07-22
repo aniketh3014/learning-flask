68861de5cc3a8f5ed142a94a57bd78ddad31c493
Aniket <ghoshaniket1000@gmail.com>
2023-07-22 15:33:16 +0530

    used sqlite3 to store the registrations in a detabase

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
-
-
+# use sqlite3
+db = sqlite3.connect("froshims.db", check_same_thread=False)
+cur = db.cursor()
 registrants = {}
 
 SPORTS = [
@@ -15,18 +17,22 @@ SPORTS = [
 def index():
     return render_template("index.html", sports=SPORTS)
 
-
 @app.route("/register", methods=["POST"])
 def register():
+    # validate submission
     name = request.form.get("name")
-    if not name:
-        return render_template("failure.html")
     sport = request.form.get("sport")
-    if sport not in SPORTS:
+    if not name or sport not in SPORTS:
         return render_template("failure.html")
-    registrants[name] = sport
-    return render_template("success.html")
+
+    # remember registrant
+    cur.execute("INSERT INTO registrants (name, sport) VALUES(?, ?)",(name, sport))
+    # confrimation
+    return redirect("/registered")
 
 @app.route("/registered")
 def registered():
-    return render_template("registered.html", registered=registrants)
+    cur.execute("SELECT * FROM registrants")
+    regi = cur.fetchall()
+    db.close()
+    return render_template("registered.html", registered=regi)
